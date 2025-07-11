7e4550a0a4105fd853a4e598950a04a7492c3e82
xxxjk <2419646890@qq.com>
2025-07-11 21:57:21 +0800

    Added some Chinese comments.

--- src/main/java/net/xxxjk/TYPE_MOON_WORLD/TYPE_MOON_WORLD.java
+++ src/main/java/net/xxxjk/TYPE_MOON_WORLD/TYPE_MOON_WORLD.java
@@ -17,26 +17,33 @@ import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
+// The value here should match an entry in the META-INF/neoforge.mods.toml file.
+//此处的值应与 META-INF/neoforge.mods.toml 文件中的条目匹配。
 @Mod(TYPE_MOON_WORLD.MOD_ID)
 public class TYPE_MOON_WORLD {
-    public static final String MOD_ID = "typemoonworld";
+    public static final String MOD_ID = "type moon world";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-
-
     public TYPE_MOON_WORLD(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
+        // Register the commonSetup method for mod loading
+        //注册 commonSetup 方法用于 mod 加载。
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in.
+        //注册我们感兴趣的服务器和其他游戏活动。
+
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        //请注意，当且仅当我们希望*此*类（ExampleMod）直接响应事件时，这才是必要的。
+
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        //如果此类中没有 @SubscribeEvent 注释的函数（如下面的 onServerStarting()），请不要添加此行。
         NeoForge.EVENT_BUS.register(this);
 
         // Register the item to a creative tab
+        //将物品注册到创意标签。
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        //注册我们的 mod 的 ModConfigSpec，以便 FML 可以为我们创建并加载配置文件。
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
@@ -45,17 +52,20 @@ public class TYPE_MOON_WORLD {
     }
 
     // Add the example block item to the building blocks tab
+    //将示例块项目添加到构建块选项卡。
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
+    //您可以使用 SubscribeEvent 并让事件总线发现要调用的方法。
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
 
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    //您可以使用 EventBusSubscriber 自动注册带有 @SubscribeEvent 注释的类中的所有静态方法。
     @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
