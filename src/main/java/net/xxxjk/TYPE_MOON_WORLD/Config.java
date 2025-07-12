7aa988f55efa815bb13850339e5b010a2d8574f3
xxxjk <2419646890@qq.com>
2025-07-12 23:38:31 +0800

    add some items: Magic Fragments Holy Shroud

--- src/main/java/net/xxxjk/TYPE_MOON_WORLD/Config.java
+++ src/main/java/net/xxxjk/TYPE_MOON_WORLD/Config.java
@@ -1,9 +1,5 @@
 package net.xxxjk.TYPE_MOON_WORLD;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -12,14 +8,17 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // 一个示例配置类，这不是必需的，但最好有一个，以便让你的配置保持条理。
 
 // Demonstrates how to use Neo's config APIs
 // 演示如何使用 Neo 的配置 API。
 @EventBusSubscriber(modid = TYPE_MOON_WORLD.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
-public class Config
-{
+public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     private static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
@@ -46,14 +45,12 @@ public class Config
     public static String magicNumberIntroduction;
     public static Set<Item> items;
 
-    private static boolean validateItemName(final Object obj)
-    {
+    private static boolean validateItemName(final Object obj) {
         return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
     }
 
     @SubscribeEvent
-    static void onLoad(final ModConfigEvent event)
-    {
+    static void onLoad(final ModConfigEvent event) {
         logDirtBlock = LOG_DIRT_BLOCK.get();
         magicNumber = MAGIC_NUMBER.get();
         magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
--- src/main/java/net/xxxjk/TYPE_MOON_WORLD/TYPE_MOON_WORLD.java
+++ src/main/java/net/xxxjk/TYPE_MOON_WORLD/TYPE_MOON_WORLD.java
@@ -1,9 +1,7 @@
 package net.xxxjk.TYPE_MOON_WORLD;
 
-import org.slf4j.Logger;
-
 import com.mojang.logging.LogUtils;
-
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -16,6 +14,8 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.xxxjk.TYPE_MOON_WORLD.item.ModItems;
+import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file.
 //此处的值应与 META-INF/neoforge.mods.toml 文件中的条目匹配。
@@ -39,6 +39,8 @@ public class TYPE_MOON_WORLD {
         //如果此类中没有 @SubscribeEvent 注释的函数（如下面的 onServerStarting()），请不要添加此行。
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         //将物品注册到创意标签。
         modEventBus.addListener(this::addCreative);
@@ -54,7 +56,10 @@ public class TYPE_MOON_WORLD {
     // Add the example block item to the building blocks tab
     //将示例块项目添加到构建块选项卡。
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.MAGIC_FRAGMENTS);
+            event.accept(ModItems.HOLY_SHROUD);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
