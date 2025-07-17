866f182715b5de7db5ac5fe0ec0b94622fed8af9
xxxjk <2419646890@qq.com>
2025-07-17 17:41:39 +0800

    add some stones: carved_emerald carved_ruby carved_sapphire carved_topaz carved_white_gemstone
    raw_emerald
    raw_ruby
    raw_sapphire
    raw_topaz
    raw_white_gemstone
    
    emerald_block
    emerald_mine
    ruby_block
    ruby_mine
    sapphire_block
    sapphire_mine
    topaz_block
    topaz_mine
    white_gemstone_block
    white_gemstone_mine

--- src/main/java/net/xxxjk/TYPE_MOON_WORLD/item/ModItems.java
+++ src/main/java/net/xxxjk/TYPE_MOON_WORLD/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.xxxjk.TYPE_MOON_WORLD.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -13,7 +14,30 @@ public class ModItems {
     public static final DeferredItem<Item> MAGIC_FRAGMENTS = ITEMS.register("magic_fragments",
             () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> HOLY_SHROUD = ITEMS.register("holy_shroud",
+            () -> new Item(new Item.Properties().rarity(Rarity.UNCOMMON)));
+
+    public static final DeferredItem<Item> CARVED_EMERALD = ITEMS.register("carved_emerald",
             () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CARVED_RUBY = ITEMS.register("carved_ruby",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CARVED_SAPPHIRE = ITEMS.register("carved_sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CARVED_TOPAZ = ITEMS.register("carved_topaz",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CARVED_WHITE_GEMSTONE = ITEMS.register("carved_white_gemstone",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> RAW_EMERALD = ITEMS.register("raw_emerald",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_RUBY = ITEMS.register("raw_ruby",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_TOPAZ = ITEMS.register("raw_topaz",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_WHITE_GEMSTONE = ITEMS.register("raw_white_gemstone",
+            () -> new Item(new Item.Properties()));
+
 
 
 
---
+++ src/main/java/net/xxxjk/TYPE_MOON_WORLD/item/custom/ChiselItem.java
@@ -0,0 +1,4 @@
+package net.xxxjk.TYPE_MOON_WORLD.item.custom;
+
+public class ChiselItem {
+}
