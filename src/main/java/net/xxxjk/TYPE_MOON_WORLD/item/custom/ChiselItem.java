bda75b7db158978e1922957561757fc4032d4b59
xxxjk <2419646890@qq.com>
2025-07-17 18:44:36 +0800

    add some things: chisel

--- src/main/java/net/xxxjk/TYPE_MOON_WORLD/item/custom/ChiselItem.java
+++ src/main/java/net/xxxjk/TYPE_MOON_WORLD/item/custom/ChiselItem.java
@@ -1,4 +1,25 @@
 package net.xxxjk.TYPE_MOON_WORLD.item.custom;
 
-public class ChiselItem {
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class ChiselItem extends Item {
+    public ChiselItem(Properties properties) {
+        super(properties);
+    }
+
+    public boolean hasCraftingRemainingItem(@NotNull ItemStack stack) {
+        return true;
+    }
+
+    public @NotNull ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+        ItemStack retrieval = new ItemStack(this);
+        retrieval.setDamageValue(itemstack.getDamageValue() + 1);
+        return retrieval.getDamageValue() >= retrieval.getMaxDamage() ? ItemStack.EMPTY : retrieval;
+    }
+
+    public boolean isRepairable(@NotNull ItemStack itemstack) {
+        return false;
+    }
 }
