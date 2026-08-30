d4c10284c8980ab9ae590f7ef73036d059cd65b4
xxxjk <2419646890@qq.com>
2026-08-31 00:45:08 +0800

    删除了间桐樱附属的强行整合

--- sakura-addon/src/main/java/io/github/typemoonaddon/effect/SpiritualDamageEffect.java
+++
@@ -1,8 +0,0 @@
-package io.github.typemoonaddon.effect;
-
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectCategory;
-
-public final class SpiritualDamageEffect extends MobEffect {
-    public SpiritualDamageEffect() { super(MobEffectCategory.HARMFUL, 0x7A102B); }
-}
